8598b4ce931124f8ade2351525be660c6a1c5073
Gustavo Freitas <ac.gbpfreitas@gmail.com>
2023-09-09 17:01:50 -0700

    Main: Main Content Features component created

---
+++ src/components/MainContent/FeaturesSection.vue
@@ -0,0 +1,56 @@
+<template>
+    <article class="features row" id="features">
+        <header class="row-title">
+            <h2>Some Skills and Features</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat pariatur dicta eius laborum optio quod.
+            </p>
+        </header>
+        <section class="content">
+            <ul class="circles">
+                <li>
+                    <p><i class="fi-xwsuxl-laptop-solid"></i></p>
+                    <div>
+                        <h3>Responsive Website</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione, quam animi dicta consequuntur quis recusandae?
+                        </p>
+                    </div>
+                </li>
+                <li>
+                    <p><i class="fi-xnsuxl-settings-solid"></i></p>
+                    <div>
+                        <h3>Reusable and maintanable code</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione, quam animi dicta consequuntur quis recusandae?
+                        </p>
+                    </div>
+                </li>
+                <li>
+                    <p><i class="fa-solid fa-a"></i></p>
+                    <div>
+                        <h3>CSS Styling</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione, quam animi dicta consequuntur quis recusandae?
+                        </p>
+                    </div>
+                </li>
+                <li>
+                    <p><i class="fi-xnsuxl-user-solid"></i></p>
+                    <div>
+                        <h3>Team Work</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione, quam animi dicta consequuntur quis recusandae?
+                        </p>
+                    </div>
+                </li>
+            </ul>
+        </section>
+    </article>
+</template>
+
+<script>
+    export default {
+        name: "FeaturesSection"
+    }
+</script>
