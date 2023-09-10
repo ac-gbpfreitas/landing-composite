b75c8ccd3360e9d96f15e99982079f46167e8289
Gustavo Freitas <ac.gbpfreitas@gmail.com>
2023-09-10 00:48:46 -0700

    Main: About section created

---
+++ src/components/MainContent/AboutSection.vue
@@ -0,0 +1,60 @@
+<template>
+    <article class="about row" id="about">
+        <header class="row-title">
+            <h2>About Me</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat pariatur dicta eius laborum optio quod.
+            </p>
+        </header>
+        <section class="content">
+            <div class="left">
+                <h3>Full Stack DEVELOPERS</h3>
+                <h4>WEB DESIGN & DEVELOPMENT</h4>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores laudantium, obcaecati, accusantium excepturi dolores possimus deleniti ex, nulla harum ratione placeat distinctio odio officiis ipsam minima laborum libero praesentium hic ullam id. Nobis, nesciunt maxime.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt vitae, natus quisquam nulla reprehenderit doloribus eaque aperiam quaerat aspernatur deleniti enim. Perspiciatis nostrum, voluptas magnam, corporis maiores esse ex doloremque fugit, veritatis neque dignissimos possimus!
+                </p>
+            </div>
+            <div class="right">
+                <ul class="bars">
+                    <li class="html5">
+                        <div>
+                            <small>HTML5</small>
+                            <small>100%</small>
+                        </div>
+                        <p><span></span></p>
+                    </li>
+                    <li class="css3">
+                        <div>
+                            <small>CSS3</small>
+                            <small>100%</small>
+                        </div>
+                        <p><span></span></p>
+                    </li>
+                    <li class="jquery">
+                        <div>
+                            <small>JQUERY</small>
+                            <small>60%</small>
+                        </div>
+                        <p><span></span></p>
+                    </li>
+                    <li class="vue">
+                        <div>
+                            <small>VUE JS</small>
+                            <small>70%</small>
+                        </div>
+                        <p><span></span></p>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    </article>
+</template>
+
+<script>
+export default {
+    name: "AboutSection"
+}
+</script>
