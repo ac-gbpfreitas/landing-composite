4babccb73b6834fc5f47a113e90cf8fd9759074d
Gustavo Freitas <ac.gbpfreitas@gmail.com>
2023-09-10 00:51:17 -0700

    Main: Work section created

---
+++ src/components/MainContent/WorkSection.vue
@@ -0,0 +1,87 @@
+<template>
+    <article class="work row" id="work">
+        <header class="row-title">
+            <h2>Work Experience</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat pariatur dicta eius laborum optio quod.
+            </p>
+        </header>
+        <section class="content">
+            <figure>
+                <img src="./assets/img/company-01.jpg" alt="Company1">
+                <figcaption>
+                    <div>
+                        <h3>PHP Software Developer Junior</h3>
+                        <h4>Company Name 1</h4>
+                        <h4>Mar/2022 (Currently Remote)</h4>
+                    </div>
+                    <ul>
+                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eum unde quasi!</li>
+                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eum unde quasi!</li>
+                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eum unde quasi!</li>
+                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eum unde quasi!</li>
+                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eum unde quasi!</li>
+                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eum unde quasi!</li>
+                    </ul>
+                </figcaption>
+            </figure>
+            <figure>
+                <img src="./assets/img/company-02.jpg" alt="Company2">
+                <figcaption>
+                    <div>
+                        <h3>Teacher Assistant</h3>
+                        <h4>Company Name 2 (Jan/2022 - Jul/2023)</h4>
+                    </div>
+                    <ul>
+                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eum unde quasi!</li>
+                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eum unde quasi!</li>
+                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eum unde quasi!</li>
+                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eum unde quasi!</li>
+                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eum unde quasi!</li>
+                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eum unde quasi!</li>
+                    </ul>
+                </figcaption>
+            </figure>
+            <figure>
+                <img src="./assets/img/company-03.jpg" alt="Company3">
+                <figcaption>
+                    <div>
+                        <h3>Junior Software Developer</h3>
+                        <h4>Company Name 3 (Sep/2021 - Nov/2021)</h4>
+                    </div>
+                    <ul>
+                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eum unde quasi!</li>
+                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eum unde quasi!</li>
+                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eum unde quasi!</li>
+                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eum unde quasi!</li>
+                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eum unde quasi!</li>
+                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eum unde quasi!</li>
+                    </ul>
+                </figcaption>
+            </figure>
+            <figure>
+                <img src="./assets/img/company-04.jpg" alt="Company4">
+                <figcaption>
+                    <div>
+                        <h3>PHP Software Developer Junior</h3>
+                        <h4>Company Name 4 (Dec/2015 - Feb/201)</h4>
+                    </div>
+                    <ul>
+                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eum unde quasi!</li>
+                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eum unde quasi!</li>
+                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eum unde quasi!</li>
+                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eum unde quasi!</li>
+                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eum unde quasi!</li>
+                        <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eum unde quasi!</li>
+                    </ul>
+                </figcaption>
+            </figure>
+        </section>
+    </article>
+</template>
+
+<script>
+    export default {
+        name: "WorkSection"
+    }
+</script>
