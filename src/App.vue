a557205eb444f0b0db6a200086585d71ea6aca8e
Gustavo Freitas <ac.gbpfreitas@gmail.com>
2023-09-10 01:57:50 -0700

    Main: Composite website done

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NavComponent />
+  <HeaderComponent />
+  <MainContent />
+  <FooterComponent />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FooterComponent from './components/FooterComponent.vue';
+import HeaderComponent from './components/HeaderComponent.vue';
+import MainContent from './components/MainContent.vue';
+import NavComponent from './components/NavComponent.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    NavComponent,
+    HeaderComponent,
+    MainContent,
+    FooterComponent
+  },
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+//npm install --save-dev node-sass sass-loader
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
